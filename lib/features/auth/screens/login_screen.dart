4aebb295875ffc95c4db7d00d5c15cc795860d2f
step-be-step <zasweq41@gmail.com>
2023-02-15 21:12:15 +0300

    first commit

---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:socibip/core/common/sing_in_button.dart';
+import 'package:socibip/core/constants/constants.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          Constants.logoPath,
+          height: 40,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Пропустить',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 30),
+          const Text(
+            "Мера присуща",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.5,
+            ),
+          ),
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              Constants.loginEmotePath,
+              height: 400,
+            ),
+          ),
+          const SizedBox(height: 20),
+          const SignInButton(),
+        ],
+      ),
+    );
+  }
+}
