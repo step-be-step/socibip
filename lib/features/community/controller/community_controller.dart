7f6b324969a4635eab9ad20901a89ccc1861408d
step-be-step <zasweq41@gmail.com>
2023-03-30 12:36:49 +0300

    Creating Communities and Storing to Firebase

---
+++ lib/features/community/controller/community_controller.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
+import 'package:socibip/core/constants/constants.dart';
+import 'package:socibip/core/utils.dart';
+import 'package:socibip/features/auth/controller/auth_controller.dart';
+import 'package:socibip/features/community/repository/community_repository.dart';
+import 'package:socibip/models/community_model.dart';
+
+final communityControllerProvider =
+    StateNotifierProvider<CommunityController, bool>((ref) {
+  final communityRepository = ref.watch(communityRepositoryProvider);
+  return CommunityController(
+      communityRepository: communityRepository, ref: ref);
+});
+
+class CommunityController extends StateNotifier<bool> {
+  final Ref _ref;
+  final CommunityRepository _communityRepository;
+  CommunityController(
+      {required CommunityRepository communityRepository, required Ref ref})
+      : _communityRepository = communityRepository,
+        _ref = ref,
+        super(false);
+
+  void createCommunity(String name, BuildContext context) async {
+    state = true;
+    final uid = _ref.read(userProvider)?.uid ?? '';
+    Community community = Community(
+      id: name,
+      name: name,
+      banner: Constants.bannerDefault,
+      avatar: Constants.avatarDefault,
+      members: [uid],
+      mods: [uid],
+    );
+
+    final res = await _communityRepository.createCommunity(community);
+    state = false;
+    res.fold((l) => showSnackBar(context, l.message), (r) {
+      showSnackBar(context, 'Обсуждение создано!');
+      Routemaster.of(context).pop();
+    });
+  }
+}
--- lib/features/community/screen/create_community_screen.dart
+++ lib/features/community/screen/create_community_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:socibip/core/common/loader.dart';
+import 'package:socibip/features/community/controller/community_controller.dart';
 
 class CreateCommunityScreen extends ConsumerStatefulWidget {
   const CreateCommunityScreen({super.key});
@@ -18,47 +20,60 @@ class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
     communityNameContoller.dispose();
   }
 
+  void createCommunity() {
+    ref
+        .read(communityControllerProvider.notifier)
+        .createCommunity(communityNameContoller.text.trim(), context);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(communityControllerProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Создать обсуждение'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text('Название обсуждения'),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: communityNameContoller,
-              decoration: const InputDecoration(
-                hintText: 'r/название_обсуждения',
-                filled: true,
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.all(18),
+      body: isLoading
+          ? const Loader()
+          : Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  const Align(
+                    alignment: Alignment.topLeft,
+                    child: Text('Название обсуждения'),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: communityNameContoller,
+                    decoration: const InputDecoration(
+                      hintText: 'r/название_обсуждения',
+                      filled: true,
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(18),
+                    ),
+                    maxLength: 21,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    onPressed: createCommunity,
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text(
+                      'Создать обсуждение',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  )
+                ],
               ),
-              maxLength: 21,
             ),
-            const SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              child: const Text('Создать обсуждение', style: TextStyle(fontSize: 17),),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
